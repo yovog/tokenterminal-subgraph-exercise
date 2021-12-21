4a8ffbbff36400aa0a5d38dd67fdecdd749e222d
Yovo Gitsov <yovogi@gmail.com>
2021-12-21 10:37:28 +0200

    Task 3 implementation 21.12

--- task-3/src/mapping.ts
+++ task-3/src/mapping.ts
@@ -4,22 +4,21 @@ import { TransferEvent } from '../generated/schema'
 export function handleTransfer(event: Transfer): void {
   // Create a new TransferEvent entity based on the transaction hash of the event
   // let ...
-  let transferEvent=new TransferEvent();
+  let transferEvent=new TransferEvent(event.transaction.hash.toString());
 
   let contract = ENSToken.bind(event.address)
- 
+  
   // Populate transferEvent fields based on event metadata
   // transferEvent.timestamp = 
-  transferEvent.id = contract.id()
-  transferEvent.timestamp= contract.timestamp();
-  transferEvent.block = contract.block()
-  transferEvent.fromAddress = contract.fromAddress()
-  transferEvent.toAddress = contract.fromAddress()
-  transferEvent.transferAmount=contract.transferAmount();
-
-
-  transferEvent.fromBalance = contract.balanceOf()
-  transferEvent.toBalance = contract.balanceOf()
+  transferEvent.id = event.transaction.hash.toString()
+  transferEvent.timestamp= event.block.timestamp.toI32()
+  transferEvent.block = event.block.number
+  transferEvent.fromAddress = event.params.from
+  transferEvent.toAddress = event.params.to
+  transferEvent.transferAmount=event.params.value
+  transferEvent.fromBalance = contract.balanceOf(event.params.from)
+  transferEvent.toBalance = contract.balanceOf(event.params.to)
+  transferEvent.save()
   
 
   // Use the balanceOf public read-only function of the ENSToken 
